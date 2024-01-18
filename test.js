7425948cdb0262351e4b2fe8b6c5ba916661f0d1
ThienBk <88071364+ThienBk@users.noreply.github.com>
2024-01-18 14:02:36 +0700

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,1 @@
-var tag2="Size",threeDot=document.getElementsByClassName("overflow_menu"),tag="Hide";function run(){setTimeout((function(){for(var e=document.querySelectorAll(".popmenu_listItem_button"),t=0;t<e.length;t++)e[t].innerText.includes(tag)&&(e[t].click(),console.log("click"));console.log("running1"),setTimeout((function(){for(var e=document.querySelectorAll(".popmenu_listItem_button"),t=0;t<e.length;t++)e[t].innerText.includes(tag2)&&(e[t].click(),console.log("click"));console.log("running2")}),"0")}),"0")}for(var i=0;i<threeDot.length;i++)threeDot[i].addEventListener("click",run);var Tags=document.getElementsByClassName("overflow_menu");for(i=0;i<=Tags.length;i++)Tags[i].style.width="40px";
-
-
 
